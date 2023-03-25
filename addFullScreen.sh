d561ce7d78ce9c40382088ccbabbeb57825199ea
RetroDISTORT <rdgc_95@hotmail.es>
2023-03-24 18:36:40 -0600

    Added full screen mode

---
+++ addFullScreen.sh
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+                function fullScreen(){
+                        document.getElementById("gm4html5_div_id").requestFullscreen();
+                }
+        </script>
+        <button onclick="fullScreen();">Full Screen Mode</button>
